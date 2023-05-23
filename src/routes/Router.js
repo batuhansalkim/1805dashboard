e8b950acc01640c50a7177d6c7ab24af73119eed
batuhan <batusalkim11@gmail.com>
2023-05-23 19:11:11 +0300

    update

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,14 +1,15 @@
 import React from "react";
-import { BrowserRouter,Routes,Route} from "react-router-dom";
+import { BrowserRouter,Routes,Route,Navigate} from "react-router-dom";
 import Dashboard from "../pages/Dashboard";
 import Booking from "../pages/Booking";
 import SellCar from "../pages/SellCar";
 import Settings from "../pages/Settings";
 
 
-function Router(){                                                                                                                                                          
+function Router(){                                                 
     return <BrowserRouter>
         <Routes>
+        <Route path="/" element={<Navigate to="/dashboard" element={<Dashboard/>}/> }/>
         <Route path="/dashboard" element={<Dashboard/>}/>
         <Route path="/bookings" element={<Booking/>}/>
         <Route path="/sell-car" element={<SellCar/>}/>
