ceef544519fa6003f548fe188e1a5719b8dc0aa9
batuhan <batusalkim11@gmail.com>
2023-05-25 01:06:34 +0300

    update

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -7,17 +7,21 @@ import "../TopNav/top-nav.css";
 function TopNav() {
   return (
     <div className="top__nav">
+
       <div className="top__nav-wrapper">
+
         <div className="search__box">
           <input type="text" placeholder='search or type' />
           <span><i className='ri-search-line'></i></span>
         </div>
+
         <div className="top__nav-right">
           <span className='notification'><i className='ri-notification-3-line'></i></span>
           <div className="profile">
             <Link to="/settings"><img src={profileImg} alt="" /></Link>
           </div>
         </div>
+        
       </div>
     </div>
   )
