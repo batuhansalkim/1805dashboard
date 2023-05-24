3e247b2883fb91ee228e561b5000d98c0801719a
batuhan <batusalkim11@gmail.com>
2023-05-24 20:35:05 +0300

    update

--- src/components/TopNav/TopNav.js
+++ src/components/TopNav/TopNav.js
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import profileImg from "../../assets/images/profile-02.png";
 function TopNav() {
   return (
     <div className="top__nav">
@@ -7,6 +8,12 @@ function TopNav() {
         <div className="search__box">
           <input type="text" placeholder='search or type' />
           <span><i className='ri-search-line'></i></span>
+        </div>
+        <div className="top__nav-right">
+          <span className='notification'><i className='ri-notification-3-line'></i></span>
+          <div className="profile">
+            <Link to="/settings"><img src={profileImg} alt="" /></Link>
+          </div>
         </div>
       </div>
     </div>
