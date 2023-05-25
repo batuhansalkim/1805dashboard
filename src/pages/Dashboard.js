a751246e45f06c96b02d091abbd0fe9ff324d17f
batuhan <batusalkim11@gmail.com>
2023-05-26 02:44:44 +0300

    2605batu

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -2,6 +2,8 @@ import React from "react";
 import "../styles/dashboard.css";
 import SingleCard from "../reuseable/SingleCard";
 import MileChart from "../charts/MileChart";
+import CarStatsChart from "../charts/CarStatsChart";
+import image1 from "../assets/images/mini-car-01.png";
 function Dashboard(){
     return(
         <div className="dashboard">
@@ -11,13 +13,44 @@ function Dashboard(){
                 </div>
                 <div className="statics">
                     <div className="stats">
-                        <h3 className="stats__title">Miles Statics</h3>
+                        <h3 className="stats__title">Miles Statistics</h3>
                         <MileChart/>
                     </div>
 
-                    
+                    <div className="stats">
+                        <h3 className="stats__title">Car Statistics</h3>
+                        <CarStatsChart />
+                    </div>
                 </div>
 
+
+                <div className="recommed__cars-wrapper">
+                    <div className="recommend__car-card">
+
+                        <div className="recomend__car-top">
+                            <h5><span><i className="ri-refresh-line"></i></span>74% Recommended</h5>
+                        </div>
+
+
+                        <div className="recommend__car-img">
+                            <img src={image1} alt="" />
+                        </div>
+
+
+                        <div className="reccomend__car-bottom">
+                            <h4>Minni Cooper</h4>
+                            
+                            <div className="recommend__car-other">
+                                <div>
+                                    <p><span><i className="ri-repeat-line"></i></span>130k</p>
+
+                                    <p><span><i className="ri-timer-flash-line"></i></span>130k</p>
+                                </div>
+                                <span>$30/h</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
