92f585bad4e170c742afaf99aed429a42333ed1c
batuhan <batusalkim11@gmail.com>
2023-05-26 02:54:16 +0300

    updatenew

---
+++ src/components/UI/RecommendCarCard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RecommendCarCard() {
+  return (
+    <div>RecommendCarCard</div>
+  )
+}
+
+export default RecommendCarCard
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -25,31 +25,7 @@ function Dashboard(){
 
 
                 <div className="recommed__cars-wrapper">
-                    <div className="recommend__car-card">
-
-                        <div className="recomend__car-top">
-                            <h5><span><i className="ri-refresh-line"></i></span>74% Recommended</h5>
-                        </div>
-
-
-                        <div className="recommend__car-img">
-                            <img src={image1} alt="" />
-                        </div>
-
-
-                        <div className="reccomend__car-bottom">
-                            <h4>Minni Cooper</h4>
-                            
-                            <div className="recommend__car-other">
-                                <div>
-                                    <p><span><i className="ri-repeat-line"></i></span>130k</p>
-
-                                    <p><span><i className="ri-timer-flash-line"></i></span>130k</p>
-                                </div>
-                                <span>$30/h</span>
-                            </div>
-                        </div>
-                    </div>
+                    
                 </div>
             </div>
         </div>
