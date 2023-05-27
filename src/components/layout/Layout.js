f2546bda0e25259d930f76fde61a773b70e9b412
batuhan <batusalkim11@gmail.com>
2023-05-27 16:12:34 +0300

    update

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -3,13 +3,15 @@ import Router from "../../routes/Router";
 import Sidebar from "../Sidebar/Sidebar";
 import TopNav from "../TopNav/TopNav";
 import Dashboard from "../../pages/Dashboard";
+import Booking from "../../pages/Booking";
 
 function Layout(){
     return <div className="layout">
         <Sidebar/>
         <div className="main__layout">
             <TopNav/>
-            
+            {/* <Dashboard/> */}
+            <Booking/>
         <div className="content">
             <Router />
                 
